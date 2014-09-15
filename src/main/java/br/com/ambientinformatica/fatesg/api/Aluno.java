b69470d28357bb1f0f9f7050ed668975edf61323
rodrigo <rodrigo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-09-15 13:12:58 +0000

    Sem herança

--- src/main/java/br/com/ambientinformatica/fatesg/api/Aluno.java
+++ src/main/java/br/com/ambientinformatica/fatesg/api/Aluno.java
@@ -2,6 +2,7 @@ package br.com.ambientinformatica.fatesg.api;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,22 +12,142 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
 @Entity
-public class Aluno implements Serializable{
+public class Aluno implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(generator = "aluno_seq", strategy = GenerationType.SEQUENCE)
-	@SequenceGenerator(name = "aluno_seq", sequenceName = "aluno_seq", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(generator = "pessoa_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "pessoa_seq", sequenceName = "pessoa_seq", allocationSize = 1, initialValue = 1)
 	private Long id;
 
-	private String certificado2Grau;
+	private String nome;
 	
+	@Column(unique = true)
+	private Long cpfCnpj;
+
+	private String rg;
+
+	@Enumerated(EnumType.STRING)
+	private EnumTipoSexo tipoSexo;
+
+	private String tituloEleitor;
+
+	private String reservista;
+
+	private String telefone;
+
+	private String celular;
+
+	private String email;
+
+	private String endereco;
+
+	private String municipio;
+
+	private String uf;
+
+	private String cep;
+
+	private String certificado2Grau;
+
 	@Enumerated(EnumType.STRING)
 	private EnumStatusAluno status;
 
-	public Long getId() {
-		return id;
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(Long cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getTituloEleitor() {
+		return tituloEleitor;
+	}
+
+	public void setTituloEleitor(String tituloEleitor) {
+		this.tituloEleitor = tituloEleitor;
+	}
+
+	public String getReservista() {
+		return reservista;
+	}
+
+	public void setReservista(String reservista) {
+		this.reservista = reservista;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getMunicipio() {
+		return municipio;
+	}
+
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
 	public String getCertificado2Grau() {
@@ -35,8 +156,16 @@ public class Aluno implements Serializable{
 
 	public void setCertificado2Grau(String certificado2Grau) {
 		this.certificado2Grau = certificado2Grau;
-	}	
-	
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public EnumTipoSexo getTipoSexo() {
+		return tipoSexo;
+	}
+
 	public EnumStatusAluno getStatus() {
 		return status;
 	}
@@ -45,11 +174,30 @@ public class Aluno implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((celular == null) ? 0 : celular.hashCode());
+		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
 		result = prime
 				* result
 				+ ((certificado2Grau == null) ? 0 : certificado2Grau.hashCode());
+		result = prime * result + ((cpfCnpj == null) ? 0 : cpfCnpj.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((endereco == null) ? 0 : endereco.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((municipio == null) ? 0 : municipio.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result
+				+ ((reservista == null) ? 0 : reservista.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
 		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result
+				+ ((telefone == null) ? 0 : telefone.hashCode());
+		result = prime * result
+				+ ((tipoSexo == null) ? 0 : tipoSexo.hashCode());
+		result = prime * result
+				+ ((tituloEleitor == null) ? 0 : tituloEleitor.hashCode());
+		result = prime * result + ((uf == null) ? 0 : uf.hashCode());
 		return result;
 	}
 
@@ -62,19 +210,81 @@ public class Aluno implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Aluno other = (Aluno) obj;
+		if (celular == null) {
+			if (other.celular != null)
+				return false;
+		} else if (!celular.equals(other.celular))
+			return false;
+		if (cep == null) {
+			if (other.cep != null)
+				return false;
+		} else if (!cep.equals(other.cep))
+			return false;
 		if (certificado2Grau == null) {
 			if (other.certificado2Grau != null)
 				return false;
 		} else if (!certificado2Grau.equals(other.certificado2Grau))
+			return false;
+		if (cpfCnpj == null) {
+			if (other.cpfCnpj != null)
+				return false;
+		} else if (!cpfCnpj.equals(other.cpfCnpj))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
 			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (municipio == null) {
+			if (other.municipio != null)
+				return false;
+		} else if (!municipio.equals(other.municipio))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (reservista == null) {
+			if (other.reservista != null)
+				return false;
+		} else if (!reservista.equals(other.reservista))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
 		if (status != other.status)
 			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		if (tipoSexo != other.tipoSexo)
+			return false;
+		if (tituloEleitor == null) {
+			if (other.tituloEleitor != null)
+				return false;
+		} else if (!tituloEleitor.equals(other.tituloEleitor))
+			return false;
+		if (uf == null) {
+			if (other.uf != null)
+				return false;
+		} else if (!uf.equals(other.uf))
+			return false;
 		return true;
-	}		
+	}	
 	
 }
