f451e496549fba7fb59cce5cacb79ba156f9a467
edjalma <edjalma@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-11-20 13:30:25 +0000

    Adicionado Teste

--- trunk/fatesg-api/src/test/java/br/com/ambientinformatica/fatesg/api/dao/CursoDaoJpaIT.java
+++ trunk/fatesg-api/src/test/java/br/com/ambientinformatica/fatesg/api/dao/CursoDaoJpaIT.java
@@ -13,78 +13,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import br.com.ambientinformatica.fatesg.api.entidade.Curso;
+
 import com.solutioin.test.UtilSql;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value="classpath:context.xml")
 public class CursoDaoJpaIT {
-   
-   @Autowired
-   private CursoDao cursoDao;
-   
-   @Autowired
-   private DataSource ds;
-   
-   private static boolean iniciado;
-   
-   @Before
-   public void iniciar(){
-      if(!iniciado){
-         try {
-            Connection con = ds.getConnection();
-            UtilSql.executarSql(con, "sql/cursos.sql");
-            con.close();
-            iniciado = true;
-         } catch (Exception e) {
-            e.printStackTrace();
-         }
-      }
-   }
-   
-   @Test
-   public void listarParceiroPorNomeNomeCurtoCpfCnpj(){
 
-      try{
-         List<Parceiro> parceiros1 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj("Maria", true, null, null, null);
-         Assert.assertEquals(1, parceiros1.size());
-         
-         List<Parceiro> parceiros2 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj("Valor", true, true, true, true);
-         Assert.assertEquals(1, parceiros2.size());
-         
-         List<Parceiro> parceiros3 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj("111.111.222-11", true, null, null, null);
-         Assert.assertEquals(1, parceiros3.size());
-         
-         List<Parceiro> parceiros4 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj("111.111.222", true, null, null, null);
-         Assert.assertEquals(0, parceiros4.size());
-         
-         List<Parceiro> parceiros5 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj("01.123.123/0001-98", true, null, null, null);
-         Assert.assertEquals(1, parceiros5.size());
-         
-         List<Parceiro> parceiros6 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj("01.123.123/0001", true, null, null, null);
-         Assert.assertEquals(0, parceiros6.size());
-         
-         List<Parceiro> parceiros7 = parceiroDao.listarParceiroPorNomeNomeCurtoCpfCnpj(null, true, null, null, null);
-         Assert.assertEquals(3, parceiros7.size());
-      } catch (Exception e) {
-         e.printStackTrace();
-         Assert.fail();
-      }
-   }
-   
-   @Test
-   public void consultaPorCpfCnpj(){
-      try {
-         Parceiro parceiro = parceiroDao.consultaPorCpfCnpj("111.111.111-11");
-         Assert.assertNotNull(parceiro);
-         Assert.assertEquals("Maria do Carmos", parceiro.getNome());
+	@Autowired
+	private CursoDao cursoDao;
 
-         Parceiro parceiro1 = parceiroDao.consultaPorCpfCnpj("123.123.555-22");
-         Assert.assertNull(parceiro1);
+	@Autowired
+	private DataSource ds;
 
-      } catch (Exception e) {
-         e.printStackTrace();
-         Assert.fail();
-      }
-   }
+	private static boolean iniciado;
+
+	@Before
+	public void iniciar(){
+		if(!iniciado){
+			try {
+				Connection con = ds.getConnection();
+				UtilSql.executarSql(con, "sql/cursos.sql");
+				con.close();
+				iniciado = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Test
+	public void listarTodos(){
+
+		try{
+			/*
+			 * Criar outros testes para metodos existentes
+			 */
+			List<Curso> parceiros1 = cursoDao.listar();
+			Assert.assertEquals(3, parceiros1.size());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
 
 }
